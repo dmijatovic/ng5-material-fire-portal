c6b78400f466a4a7d552fd6234315568d7dbb933
Dusan Mijatovic (Linux) <d.mijatovic@dv4all.nl>
2017-10-15 01:03:37 +0200

    header profile menu added

--- src/app/layout/header.ts
+++ src/app/layout/header.ts
@@ -2,50 +2,56 @@
 import { Component, OnInit } from '@angular/core';
 
 import { LoginSvc } from '../../login/login.svc';
-import { Router, ActivatedRouteSnapshot } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.html',
-    styleUrls: ['./header.scss'],
-    host:{
-        'class':'mat-elevation-z2'
-    }
+   selector: 'app-header',
+   templateUrl: './header.html',
+   styleUrls: ['./header.scss'],
+   host: {
+      'class': 'mat-elevation-z2'
+   }
 })
 export class AppHeader implements OnInit {
-    
-    title = 'Header title';
-    
-    menuItems:any;
-    profileItems:any;
 
-    constructor(
-        private login: LoginSvc,
-        private router: Router,
-        //private route: ActivatedRouteSnapshot 
-    ){}
-    ngOnInit(){
-        this.login.getAllMenuItems()
-        .then ((d)=>{
+   title = 'Header title';
+
+   menuItems: any;
+   profileItems: any;
+
+   constructor(
+      private login: LoginSvc,
+      private router: Router,
+      private route: ActivatedRoute
+   ) { }
+   ngOnInit() {
+
+      this.login.getAllMenuItems()
+         .then((d) => {
             //debugger 
             this.menuItems = d;
             return this.login.getAllProfileOptions();
-        })
-        .then((d)=>{
+         })
+         .then((d) => {
             this.profileItems = d;
-        })
-        .catch((e)=>{
-            debugger 
+         })
+         .catch((e) => {
+            debugger
             console.error(e);
-        });
-        
-    }
+         });
 
-    logOut(){
-        //can we log out?!?
-        this.login.logOut();
-    }
-    deleteAccount(){
-        this.router.navigate(['remove']);
-    }
+      this.route.data
+         .subscribe((d)=>{
+            console.log("header route data", d);
+         });
+
+   }
+
+   logOut() {
+      //can we log out?!?
+      this.login.logOut();
+   }
+   deleteAccount() {
+      this.router.navigate(['remove']);
+   }
 }
