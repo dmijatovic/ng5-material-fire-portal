2b572d969fe571bdc71f819d914bf5e1b838f910
Dusan Mijatovic (Linux) <d.mijatovic@dv4all.nl>
2017-11-14 17:24:23 +0100

    basic animation page

--- src/app/animations/home.ts
+++ src/app/animations/home.ts
@@ -1,24 +1,50 @@
-//angular 
+//angular
 import { Component, OnInit } from '@angular/core';
 
 //service to emit/dispatch actions between app components
 import { SystemActionSvc } from '../../system/sys.action.svc';
 
+import {
+  pageFadeIn, pageFadeOut, growVertical, growHorizontal,
+  itemFade, basicEffect
+} from '../../system/system.animate';
+
 @Component({
-    selector: 'app-animate-home',
-    templateUrl: './home.html',
-    styleUrls: ['./home.scss']
+  selector: 'app-animate-home',
+  templateUrl: './home.html',
+  styleUrls: ['./home.scss'],
+  animations:[
+    pageFadeIn, pageFadeOut, growVertical, growHorizontal,
+    itemFade, basicEffect
+  ]
 })
 export class AnimateHomePage implements OnInit {
-    pageTitle = 'Animations: Home page';    
-    constructor(
-        private action: SystemActionSvc
-    ){}
-    ngOnInit() {
-        //pass title we want to use
-        this.action.dispatch({
-          type:"HEADER_TITLE",
-          payload: "dv4Demo portal: " + this.pageTitle
-        });
+  pageTitle = 'Animations: Home page';
+
+  //animation variables
+  fade:string="";
+  basic:string="";
+
+
+  constructor(
+    private action: SystemActionSvc
+  ) { }
+  ngOnInit() {
+    //pass title we want to use
+    this.action.dispatch({
+      type: "HEADER_TITLE",
+      payload: "dv4Demo portal: " + this.pageTitle
+    });
+  }
+
+  animate(key=null,val=null){
+    //debugger
+    if (key){
+      if (val){
+        this[key] = val;
+      }else{
+        this[key] = key;
       }
+    }
+  }
 }
--- src/system/system.animate.ts
+++ src/system/system.animate.ts
@@ -1,102 +1,155 @@
 
-//angular 4 
-import { trigger,state, style, 
-    transition, animate 
+// angular
+import {
+  trigger, state, style,
+  transition, animate
 } from '@angular/animations';
 
 
 /**
- * PAGE FADE IN 
+ * PAGE FADE IN
  *
  */
-export const pageFadeIn = trigger('pageFadeIn',[
-    state('hidden',
-        style({
-            opacity:'0'
-        })
-    ),
-    state('visible',
-        style({
-            opacity:'1'
-        })
-    ),
-    transition('hidden => visible',[
-        animate('150ms ease-in')
-    ]) 
+export const pageFadeIn = trigger('pageFadeIn', [
+  state('hidden',
+    style({
+      opacity: '0'
+    })
+  ),
+  state('visible',
+    style({
+      opacity: '1'
+    })
+  ),
+  transition('* => visible', [
+    animate('600ms ease-in')
+  ])
 ]);
 
 /**
  * PAGE FADE OUT
- * 
+ *
  */
-export const pageFadeOut = trigger('pageFadeOut',[
-    state('hidden',
-        style({
-            opacity:'0'
-        })
-    ),
-    state('visible',
-        style({
-            opacity:'1'
-        })
-    ),
-    transition('visible => hidden',[
-        animate('300ms ease-out')
-    ]) 
+export const pageFadeOut = trigger('pageFadeOut', [
+  state('hidden',
+    style({
+      opacity: '0'
+    })
+  ),
+  state('visible',
+    style({
+      opacity: '1'
+    })
+  ),
+  transition('* => hidden', [
+    animate('300ms ease-out')
+  ])
 ]);
 
 /**
- * GROW VERTICAL
- * 
+ * ITEM FADE
+ *
  */
-export const growVertical = trigger('growVertical',[
-    state('hidden',
-        style({
-            transform:'scale(1,0)'
-        })
-    ),
-    state('visible',
-        style({
-            transform:'scale(1,1)'
-        })
-    ),/*
+export const itemFade = trigger('itemFade', [
+  state('fadeout',
+    style({
+      opacity: '0'
+    })
+  ),
+  state('fadein',
+    style({
+      opacity: '1'
+    })
+  ),
+  transition('* => fadeout', [
+    animate('300ms ease-out')
+  ]),
+  transition('* => fadein', [
+    animate('300ms ease-in')
+  ])
+]);
+
+
+
+
+
+/**
+ * GROW VERTICAL
+ *
+ */
+export const growVertical = trigger('growVertical', [
+  state('normal',
+    style({
+      transform: 'scale(1,0)'
+    })
+  ),
+  state('large',
+    style({
+      transform: 'scale(1,1)'
+    })
+  ),/*
     transition(':enter',[
         style({
             transform:'scale(1,1)'
         }),
         animate('1s 100ms ease-in')
     ]),*/
-    transition('hidden => visible',[        
-        animate('200ms 100ms ease-in')
-    ]),
-    transition('visible => hidden',[        
-        animate('100ms ease-out')
-    ]),/*
+  transition('normal => large', [
+    animate('200ms 100ms ease-in')
+  ]),
+  transition('large => normal', [
+    animate('100ms ease-out')
+  ]),/*
     transition(':leave',[
         style({
             transform:'scale(1,0)'
         }),
         animate('1s easy-out')
-    ])*/                
+    ])*/
 ]);
 
 /**
  * GROW HORIZONTAL
- * 
+ *
  */
-export const growHorizontal = trigger('growHorizontal',[
-    state('hidden',
-        style({
-            opacity:'0'
-        })
-    ),
-    state('visible',
-        style({
-            opacity:'1'
-        })
-    ),
-    transition('hidden <=> visible',[
-        animate('350ms ease-out')
-    ])
+export const growHorizontal = trigger('growHorizontal', [
+  state('hidden',
+    style({
+      opacity: '0'
+    })
+  ),
+  state('visible',
+    style({
+      opacity: '1'
+    })
+  ),
+  transition('hidden <=> visible', [
+    animate('350ms ease-out')
+  ])
 ]);
 
+
+/**
+ * basicEffects
+ *
+ */
+export const basicEffect = trigger('basicEffect', [
+  state('grow',
+    style({
+      transform: 'scale(1.5)'
+    })
+  ),
+  state('move',
+    style({
+      transform: 'translateX(-50px) translateY(-50px)'
+    })
+  ),
+  state('spin',
+    style({
+      transform: 'rotateY(45deg) rotateZ(60deg)'
+    })
+  ),
+  transition('* => *', [
+    animate('500ms ease')
+  ])
+]);
